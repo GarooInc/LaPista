68a6760a3647f5a1bed7379763266c44154cd98f
Jimena Hernández <her21199@uvg.edu.gt>
2023-08-31 11:16:16 -0600

    changes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,24 +6,13 @@ const Footer = () => {
         <footer className="footer">
             <div className="footer-left">
                 <img src="/images/logo.png" alt="Logo" className="footer-logo" />
-                <p className="footer-description">Lorem ipsum dolor sit amet consectetur. Lacus urna consectetur nunc porttitor morbi. Tristique pellentesque id nec viverra.</p>
-                <p className="footer-copyright">© 2023 Garoo Inc. All Rights Reserved.</p>
-            </div>
-            <div className="footer-right">
-                <div className="footer-column">
-                    <h4>Menú</h4>
-                    <a href="#">About Us</a>
-                    <a href="#">We´re Hiring</a>
-                    <a href="#">Privacy Policy</a>
+                <div className="footer-description">
+                <h3>Dirección</h3>
+                <p>Km 9.5 Carretera a MuxbalGuatemala, 01051, GT,
+                (502) 31746160 info@boquiteo.com</p>
                 </div>
+            </div>             
                 <div className="footer-column">
-                    <h4>Service</h4>
-                    <a href="#">About Us</a>
-                    <a href="#">We´re Hiring</a>
-                    <a href="#">Privacy Policy</a>
-                </div>
-                <div className="footer-column">
-                    <h4>Social</h4>
                     <div className="footer-socials">
                     {/* Replace # with your social media links */}
                     <a href="#" className="footer-social">
@@ -33,14 +22,10 @@ const Footer = () => {
                         <img src="/images/tw.svg" alt="twitter" />
                     </a>
                     <a href="#" className="footer-social">
-                        <img src="/images/li.svg" alt="linkedin" />
-                    </a>
-                    <a href="#" className="footer-social">
                         <img src="/images/ig.svg" alt="Instagram" />
                     </a>
                 </div>
                 </div>
-            </div>
         </footer>
     )
 }
