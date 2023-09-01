4a987b1b9b52d1cd10918a59e20d7f156f1239df
Jimena Hernández <her21199@uvg.edu.gt>
2023-08-31 20:16:15 -0600

    Update Contact.jsx

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,9 +1,43 @@
-import React from "react"
+import React,{useState} from "react"
 import styles from "./Contact.module.css"
 import Button from "../Button/Button"
 
 
 const Contact = () => {
+
+    const [formData, setFormData] = useState({
+        name: '',
+        lastname: '',
+        email: '',
+        message: '',
+    });
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        console.log(formData);
+
+        const contacto = {contacto: formData }
+        console.log(contacto);
+
+        try {
+            const response = await fetch(import.meta.env.VITE_API_ENDPOINT, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(formData),
+            });
+
+            if (response.ok) {
+                console.log('Datos enviados a Zapier exitosamente');
+            } else {
+                console.error('Error al enviar los datos');
+            }
+        } catch (error) {
+            console.error('Hubo un error:', error);
+        }
+    };
     
     return (
         <div className={styles.container}>
@@ -17,17 +51,19 @@ const Contact = () => {
             </div>
             <div className={styles.contactinfo}>
                 <div className={styles.contactright}>
-                    <form >
-                        <input type="text" placeholder="Nombres" name="from_name" />
-                        <input type="text" placeholder="Apellidos" name="last_name" />
-                        <input type="text" placeholder="Correo" name="reply_to" />
-                        <input type="text" placeholder="Teléfono" name="cellphone" />
+                    <form onSubmit={handleSubmit}>
+                        <input type="text" placeholder="Nombres" name="from_name" value={formData.name} onChange={(e) => setFormData({ ...formData, name: e.target.value })}/>
+                        <input type="text" placeholder="Apellidos" name="last_name" value={formData.lastname} onChange={(e) => setFormData({ ...formData, lastname: e.target.value })}/>
+                        <input type="text" placeholder="Correo" name="reply_to" value={formData.email} onChange={(e) => setFormData({ ...formData, email: e.target.value })}/>
+                        <input type="text" placeholder="Teléfono" name="cellphone" value={formData.cellphone} onChange={(e) => setFormData({ ...formData, cellphone: e.target.value })}/>
                         <textarea
                             id=""
                             cols="30"
                             rows="10"
                             placeholder="Mensaje"
                             name="message"
+                            value={formData.message}
+                            onChange={(e) => setFormData({ ...formData, message: e.target.value })}
                         />
                         <Button text="Enviar" backgroundcolor="#000" colortext="#fff"/>
                     </form>
